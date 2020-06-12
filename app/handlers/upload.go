dfcf23b3cd5615220d60003a84265a92d36bd4b9
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2020-06-13 00:39:12 +0530

    Rearrange files

---
+++ app/handlers/upload.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/agathver/coffee-beans/app/utils"
+	"github.com/gin-gonic/gin"
+)
+
+// Upload creates a new handler to handle artifact uploads
+func Upload(repository, contentRoot string) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		artifactPath := strings.TrimLeft(c.Param("artifact"), "/")
+
+		if isPathInvalid(artifactPath) {
+			c.String(http.StatusBadRequest, "Invalid artifact path\n")
+			return
+		}
+
+		destPath := filepath.Clean(filepath.Join(contentRoot, repository, artifactPath))
+
+		dir := filepath.Dir(destPath)
+
+		utils.Must(os.MkdirAll(dir, 0750|os.ModeDir))
+
+		destFile, err := os.Create(destPath)
+		utils.Must(err)
+		defer utils.MustDo(destFile.Close)
+
+		_, err = io.Copy(destFile, c.Request.Body)
+		utils.Must(err)
+
+		c.String(http.StatusCreated, "OK\n")
+	}
+}
+
+func isPathInvalid(artifactPath string) bool {
+	match, _ := regexp.MatchString(`[\w][\w/.\-][\w]`, artifactPath)
+
+	if !match {
+		return true
+	}
+
+	if artifactPath != path.Clean(artifactPath) {
+		return true
+	}
+
+	return false
+}
--- main.go
+++ main.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"path"
 	"path/filepath"
-	"regexp"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/agathver/coffee-beans/config"
-	"github.com/agathver/coffee-beans/utils"
+	"github.com/agathver/coffee-beans/app/config"
+	"github.com/agathver/coffee-beans/app/handlers"
+	"github.com/agathver/coffee-beans/app/utils"
 )
 
 func main() {
@@ -29,50 +24,10 @@ func main() {
 
 	for _, repository := range cfg.Repositories() {
 		id := repository.Id()
-		r.PUT(fmt.Sprintf("/repo/%s/*artifact", id), uploadHandler(id, cfg.ContentRoot()))
+		r.PUT(fmt.Sprintf("/repo/%s/*artifact", id), handlers.Upload(id, cfg.ContentRoot()))
 		r.StaticFS(fmt.Sprintf("/repo/%s", id), gin.Dir(filepath.Join(cfg.ContentRoot(), "repositories", id), true))
 	}
 
 	addr := fmt.Sprintf("%s:%d", cfg.Server().ListenAddress(), cfg.Server().ListenPort())
 	utils.Must(r.Run(addr))
-}
-
-func uploadHandler(repository, contentRoot string) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		artifactPath := strings.TrimLeft(c.Param("artifact"), "/")
-
-		if isPathInvalid(artifactPath) {
-			c.String(http.StatusBadRequest, "Invalid artifact path\n")
-			return
-		}
-
-		destPath := filepath.Clean(filepath.Join(contentRoot, repository, artifactPath))
-
-		dir := filepath.Dir(destPath)
-
-		utils.Must(os.MkdirAll(dir, 0750|os.ModeDir))
-
-		destFile, err := os.Create(destPath)
-		utils.Must(err)
-		defer utils.MustDo(destFile.Close)
-
-		_, err = io.Copy(destFile, c.Request.Body)
-		utils.Must(err)
-
-		c.String(http.StatusCreated, "OK\n")
-	}
-}
-
-func isPathInvalid(artifactPath string) bool {
-	match, _ := regexp.MatchString(`[\w][\w/.\-][\w]`, artifactPath)
-
-	if !match {
-		return true
-	}
-
-	if artifactPath != path.Clean(artifactPath) {
-		return true
-	}
-
-	return false
 }
